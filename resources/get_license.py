597538fa72ff1369f1a64d2c362addd43237b7f6
ignormies <bryce.beagle@gmail.com>
2020-02-09 12:15:12 -0800

    Fix typo and improve documentation

--- resources/get_license.py
+++ resources/get_license.py
@@ -10,14 +10,20 @@ def main():
 
     tree = ElementTree.parse(args.xsd_path)
     root = tree.getroot()
+
+    # This happens to be where the license is located. Shouldn't change w/o a
+    # version bump
     license_element = root[0][0]
 
+    # Replace sequences of two or more spaces with a single space
     license_text = re.sub(r" +", " ", license_element.text).strip()
+
+    # Write to file
     args.output_path.write_text(license_text)
 
 
 def parse_args() -> argparse.Namespace:
-    description = "Find the license in thesesar specified Base.xsd file and " \
+    description = "Extract the license from the specified Base.xsd file and " \
                   "write it to a specified file. Incredibly basic and naive."
     parser = argparse.ArgumentParser(description=description)
     # noinspection PyTypeChecker
